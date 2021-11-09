f25920bae61941da94e138472478dec67098bccf
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-11-09 14:24:17 +0530

    edited files

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php include 'header.php '  ?>
 
-
-<!-- 
-<div class="w3ls-icons">
-		<ul style="font-size: 30px">
-			<li><a href="admin/login.php" style="color: #fff;"> Admin</a></li>
-			<li style="padding-left: 20px"><a href="user/login.php" style="color: #fff;"> User</a></li>
-		</ul>
-	</div>
-<div class="w3ls-head">
-		<h1>Online Marriage Regitration System</h1>
-</div>
-<div class="w3ls-content">
-	<div class="w3ls-headding">
-		<h2> <img src="bgimages/5b9d320a2000002d00fdde0f.jpeg"></h2>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum molestiae nisi fuga quis dicta atque corporis et qui facilis suscipit neque iste consequatur quasi, officiis inventore dolorem labore ullam sint.</p>
-	</div>
-	
-</div> -->
 <hr>
 <div class="b-example-divider"></div>
 
